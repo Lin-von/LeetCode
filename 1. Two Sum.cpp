c22d24a0f57c512e516bb9919c256e244972ac2b
Linvon <704764165@qq.com>
2017-04-28 19:44:59 +0800

    1

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,18 @@
+/*
+最容易想到的是O(nlogn)的解法，遍历一遍数组做二分查找。
+下面代码是个比较聪明的解法，复杂度只有O(n)，遍历过程中查询map中是否存在对应值，不存在则加入mao继续遍历
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int ,int > m;
+        for(int i=0;i<nums.size();i++)
+        {
+            int cur=nums[i];
+            if(m.count(target-cur)>0) return vector<int > {m[target-cur],i};
+            m[cur]=i;
+            
+        }
+    }
+};
