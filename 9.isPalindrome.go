55360c825cebacfb613f79d0d6d12ab61e77225b
Linvon <704764165@qq.com>
2019-07-31 00:32:57 +0800

    accept

---
+++ 9.isPalindrome.go
@@ -0,0 +1,18 @@
+package main
+
+func isPalindrome(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0){
+		return false
+	}
+	t := x
+	r := 0
+	for ; t != 0 && t > r; {
+		r = r * 10 + t % 10
+		t /= 10
+	}
+	if t == r || t == r/10 {
+		return true
+	} else {
+		return false
+	}
+}
