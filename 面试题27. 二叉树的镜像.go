5ea8eaa969c5979f8b5ffd595ca2541e9d27ace4
Linvon <704764165@qq.com>
2020-03-28 23:21:40 +0800

    Accept

---
+++ 面试题27. 二叉树的镜像.go
@@ -0,0 +1,12 @@
+package main
+
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	l := mirrorTree(root.Left)
+	r := mirrorTree(root.Right)
+	root.Left = r
+	root.Right = l
+	return root
+}
