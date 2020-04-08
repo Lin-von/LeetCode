0c3b3905b58bb26c48daaf0c1ed843bd2a14f7bb
Linvon <704764165@qq.com>
2020-04-08 23:55:30 +0800

    Accept

---
+++ 面试题55 - I. 二叉树的深度.go
@@ -0,0 +1,16 @@
+package main
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
