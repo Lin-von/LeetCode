5ebb84bdb1853436c537cb2ae5d2ac382dde3e6f
Linvon <704764165@qq.com>
2020-04-23 11:50:04 +0800

    Accept

---
+++ 面试题 08.11. Coin LCCI.go
@@ -0,0 +1,15 @@
+package main
+
+func waysToChange(n int) int {
+	coins := []int{1, 5, 10, 25}
+
+	dp := make([]int, n + 1)
+
+	dp[0] = 1
+	for _, coin := range coins {
+		for i := coin; i <= n; i++ {
+			dp[i] += dp[i - coin] % 1000000007
+		}
+	}
+	return dp[n] % 1000000007
+}
